bf9dafd2c99ed259610535d2b25f3f7e7337074c
Yong <y3000y7@gmail.com>
2020-11-26 21:57:18 +0900

    init

--- app/src/urlFactory.js
+++ app/src/urlFactory.js
@@ -1,12 +1,11 @@
-export function getSignalingUrl(peerId, roomId)
-{
-	const port =
-		process.env.NODE_ENV !== 'production' ?
-			window.config.developmentPort
-			:
-			window.config.productionPort;
+export function getSignalingUrl(peerId, roomId) {
+  const port =
+    process.env.NODE_ENV !== "production"
+      ? window.config.developmentPort
+      : window.config.productionPort;
 
-	const url = `wss://${window.location.hostname}:${port}/?peerId=${peerId}&roomId=${roomId}`;
+  const url = `wss://meet.wizlive.com:${port}/?peerId=${peerId}&roomId=${roomId}`;
+  // const url = `wss://${window.location.hostname}:${port}/?peerId=${peerId}&roomId=${roomId}`;
 
-	return url;
+  return url;
 }
