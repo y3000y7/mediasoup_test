2d5299ab68904b6245b2340381d65687a0057fe6
rarlaWiz <68528342+rarlaWiz@users.noreply.github.com>
2020-12-08 16:51:06 +0900

    [rarla] fullscreen view, default view

--- app/src/components/MeetingViews/Democratic.js
+++ app/src/components/MeetingViews/Democratic.js
@@ -1,252 +1,237 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {
-	spotlightPeersSelector,
-	videoBoxesSelector
-} from '../Selectors';
-import classnames from 'classnames';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Peer from '../Containers/Peer';
-import Me from '../Containers/Me';
+import React from "react";
+import { connect } from "react-redux";
+import { spotlightPeersSelector, videoBoxesSelector } from "../Selectors";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Peer from "../Containers/Peer";
+import Me from "../Containers/Me";
+import * as roomAction from "../../actions/roomActions";
+import viewFilmStripIcon from "../../images/icon-default-view.svg";
 
-const PADDING_V = 64;
+const PADDING_V = 50;
 const PADDING_H = 50;
 
 const FILL_RATE = 0.95;
 
-const styles = (theme) =>
-	({
-		root :
-		{
-			width          : '100%',
-			height         : '100%',
-			display        : 'flex',
-			flexDirection  : 'row',
-			flexWrap       : 'wrap',
-			overflow       : 'hidden',
-			justifyContent : 'center',
-			alignItems     : 'center',
-			alignContent   : 'center'
-		},
-		hiddenToolBar :
-		{
-			paddingTop : 0,
-			transition : 'padding .5s'
-		},
-		showingToolBar :
-		{
-			paddingTop : PADDING_V,
-			transition : 'padding .5s'
-		},
-		buttonControlBar :
-		{
-			paddingLeft                    : PADDING_H,
-			[theme.breakpoints.down('sm')] :
-			{
-				paddingLeft : 0
-			}
-		}
-	});
+const styles = theme => ({
+  root: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center"
+  },
+  hiddenToolBar: {
+    paddingTop: 0,
+    transition: "padding .5s"
+  },
+  showingToolBar: {
+    paddingTop: PADDING_V,
+    transition: "padding .5s"
+  },
+  buttonControlBar: {
+    paddingLeft: PADDING_H,
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: 0
+    }
+  },
+  viewFilmStripIcon: {
+    position: "absolute",
+    top: "63px",
+    right: "30px",
+    width: "30px",
+    height: "30px",
+    cursor: "pointer"
+  }
+});
 
-class Democratic extends React.PureComponent
-{
-	constructor(props)
-	{
-		super(props);
+class Democratic extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-		this.state = {};
+    this.state = {};
 
-		this.resizeTimeout = null;
+    this.resizeTimeout = null;
 
-		this.peersRef = React.createRef();
-	}
+    this.peersRef = React.createRef();
+  }
 
-	updateDimensions = () =>
-	{
-		const {
-			boxes,
-			aspectRatio,
-			buttonControlBar,
-			permanentTopBar,
-			toolbarsVisible
-		} = this.props;
+  updateDimensions = () => {
+    const {
+      boxes,
+      aspectRatio,
+      buttonControlBar,
+      permanentTopBar,
+      toolbarsVisible
+    } = this.props;
 
-		const {
-			current
-		} = this.peersRef;
+    const { current } = this.peersRef;
 
-		if (!current)
-			return;
+    if (!current) return;
 
-		if (boxes === 0)
-			return;
+    if (boxes === 0) return;
 
-		const n = boxes;
+    const n = boxes;
 
-		const width =
-			current.clientWidth - (buttonControlBar ? PADDING_H : 0);
-		const height =
-			current.clientHeight - (toolbarsVisible || permanentTopBar ? PADDING_V : 0);
+    const width = current.clientWidth - (buttonControlBar ? PADDING_H : 0);
+    const height =
+      current.clientHeight -
+      (toolbarsVisible || permanentTopBar ? PADDING_V : 0);
 
-		let x, y, space;
+    let x, y, space;
 
-		for (let rows = 1; rows <= boxes; rows = rows + 1)
-		{
-			x = width / Math.ceil(n / rows);
-			y = x / aspectRatio;
+    for (let rows = 1; rows <= boxes; rows = rows + 1) {
+      x = width / Math.ceil(n / rows);
+      y = x / aspectRatio;
 
-			if (height < (y * rows))
-			{
-				y = height / rows;
-				x = aspectRatio * y;
+      if (height < y * rows) {
+        y = height / rows;
+        x = aspectRatio * y;
 
-				break;
-			}
+        break;
+      }
 
-			space = height - (y * (rows));
+      space = height - y * rows;
 
-			if (space < y)
-				break;
-		}
+      if (space < y) break;
+    }
 
-		if (
-			Math.ceil(this.state.peerWidth) !== Math.ceil(FILL_RATE * x) ||
-			Math.ceil(this.state.peerHeight) !== Math.ceil(FILL_RATE * y)
-		)
-		{
-			this.setState({
-				peerWidth  : FILL_RATE * x,
-				peerHeight : FILL_RATE * y
-			});
-		}
-	};
+    if (
+      Math.ceil(this.state.peerWidth) !== Math.ceil(FILL_RATE * x) ||
+      Math.ceil(this.state.peerHeight) !== Math.ceil(FILL_RATE * y)
+    ) {
+      this.setState({
+        peerWidth: FILL_RATE * x,
+        peerHeight: FILL_RATE * y
+      });
+    }
+  };
 
-	componentDidMount()
-	{
-		// window.resize event listener
-		window.addEventListener('resize', () =>
-		{
-			// clear the timeout
-			clearTimeout(this.resizeTimeout);
+  componentDidMount() {
+    // window.resize event listener
+    window.addEventListener("resize", () => {
+      // clear the timeout
+      clearTimeout(this.resizeTimeout);
 
-			// start timing for event "completion"
-			this.resizeTimeout = setTimeout(() => this.updateDimensions(), 250);
-		});
+      // start timing for event "completion"
+      this.resizeTimeout = setTimeout(() => this.updateDimensions(), 250);
+    });
 
-		this.updateDimensions();
-	}
+    this.updateDimensions();
+  }
 
-	componentWillUnmount()
-	{
-		window.removeEventListener('resize', this.updateDimensions);
-	}
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
 
-	componentDidUpdate(prevProps)
-	{
-		if (prevProps !== this.props)
-			this.updateDimensions();
-	}
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) this.updateDimensions();
+  }
 
-	render()
-	{
-		const {
-			advancedMode,
-			spotlightsPeers,
-			toolbarsVisible,
-			permanentTopBar,
-			buttonControlBar,
-			hideSelfView,
-			classes
-		} = this.props;
+  render() {
+    const {
+      advancedMode,
+      spotlightsPeers,
+      toolbarsVisible,
+      permanentTopBar,
+      buttonControlBar,
+      hideSelfView,
+      classes
+    } = this.props;
 
-		const style =
-		{
-			'width'  : this.state.peerWidth ? this.state.peerWidth : 0,
-			'height' : this.state.peerHeight ? this.state.peerHeight : 0
-		};
+    const style = {
+      width: this.state.peerWidth ? this.state.peerWidth : 0,
+      height: this.state.peerHeight ? this.state.peerHeight : 0
+    };
 
-		return (
-			<div
-				className={classnames(
-					classes.root,
-					toolbarsVisible || permanentTopBar ?
-						classes.showingToolBar : classes.hiddenToolBar,
-					buttonControlBar ? classes.buttonControlBar : null
-				)}
-				ref={this.peersRef}
-			>
-				{ !hideSelfView &&
-				<Me
-					advancedMode={advancedMode}
-					spacing={6}
-					style={style}
-				/>
-				}
-				{ spotlightsPeers.map((peer) =>
-				{
-					return (
-						<Peer
-							key={peer}
-							advancedMode={advancedMode}
-							id={peer}
-							spacing={6}
-							style={style}
-						/>
-					);
-				})}
-			</div>
-		);
-	}
+    return (
+      <div
+        className={classnames(
+          classes.root,
+          toolbarsVisible || permanentTopBar
+            ? classes.showingToolBar
+            : classes.hiddenToolBar,
+          buttonControlBar ? classes.buttonControlBar : null
+        )}
+        ref={this.peersRef}
+      >
+        {!hideSelfView && (
+          <Me advancedMode={advancedMode} spacing={6} style={style} />
+        )}
+        {spotlightsPeers.map(peer => {
+          return (
+            <Peer
+              key={peer}
+              advancedMode={advancedMode}
+              id={peer}
+              spacing={6}
+              style={style}
+            />
+          );
+        })}
+        <img
+          src={viewFilmStripIcon}
+          alt="viewFilmStripIcon"
+          className={classes.viewFilmStripIcon}
+          onClick={() => {
+            this.props.setDisplayMode("filmstrip");
+          }}
+        />
+      </div>
+    );
+  }
 }
 
-Democratic.propTypes =
-{
-	advancedMode     : PropTypes.bool,
-	boxes            : PropTypes.number,
-	spotlightsPeers  : PropTypes.array.isRequired,
-	toolbarsVisible  : PropTypes.bool.isRequired,
-	hideSelfView     : PropTypes.bool.isRequired,
-	permanentTopBar  : PropTypes.bool.isRequired,
-	buttonControlBar : PropTypes.bool.isRequired,
-	toolAreaOpen     : PropTypes.bool.isRequired,
-	aspectRatio      : PropTypes.number.isRequired,
-	classes          : PropTypes.object.isRequired
+Democratic.propTypes = {
+  advancedMode: PropTypes.bool,
+  boxes: PropTypes.number,
+  spotlightsPeers: PropTypes.array.isRequired,
+  toolbarsVisible: PropTypes.bool.isRequired,
+  hideSelfView: PropTypes.bool.isRequired,
+  permanentTopBar: PropTypes.bool.isRequired,
+  buttonControlBar: PropTypes.bool.isRequired,
+  toolAreaOpen: PropTypes.bool.isRequired,
+  aspectRatio: PropTypes.number.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
-const mapStateToProps = (state) =>
-{
-	return {
-		boxes            : videoBoxesSelector(state),
-		spotlightsPeers  : spotlightPeersSelector(state),
-		toolbarsVisible  : state.room.toolbarsVisible,
-		hideSelfView     : state.room.hideSelfView,
-		permanentTopBar  : state.settings.permanentTopBar,
-		buttonControlBar : state.settings.buttonControlBar,
-		toolAreaOpen     : state.toolarea.toolAreaOpen,
-		aspectRatio      : state.settings.aspectRatio
-	};
+const mapStateToProps = state => {
+  return {
+    boxes: videoBoxesSelector(state),
+    spotlightsPeers: spotlightPeersSelector(state),
+    toolbarsVisible: state.room.toolbarsVisible,
+    hideSelfView: state.room.hideSelfView,
+    permanentTopBar: state.settings.permanentTopBar,
+    buttonControlBar: state.settings.buttonControlBar,
+    toolAreaOpen: state.toolarea.toolAreaOpen,
+    aspectRatio: state.settings.aspectRatio
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	null,
-	null,
-	{
-		areStatesEqual : (next, prev) =>
-		{
-			return (
-				prev.peers === next.peers &&
-				prev.producers === next.producers &&
-				prev.consumers === next.consumers &&
-				prev.room.spotlights === next.room.spotlights &&
-				prev.room.toolbarsVisible === next.room.toolbarsVisible &&
-				prev.room.hideSelfView === next.room.hideSelfView &&
-				prev.settings.permanentTopBar === next.settings.permanentTopBar &&
-				prev.settings.buttonControlBar === next.settings.buttonControlBar &&
-				prev.settings.aspectRatio === next.settings.aspectRatio &&
-				prev.toolarea.toolAreaOpen === next.toolarea.toolAreaOpen
-			);
-		}
-	}
+  mapStateToProps,
+  { setDisplayMode: roomAction.setDisplayMode },
+  null,
+  {
+    areStatesEqual: (next, prev) => {
+      return (
+        prev.peers === next.peers &&
+        prev.producers === next.producers &&
+        prev.consumers === next.consumers &&
+        prev.room.spotlights === next.room.spotlights &&
+        prev.room.toolbarsVisible === next.room.toolbarsVisible &&
+        prev.room.hideSelfView === next.room.hideSelfView &&
+        prev.settings.permanentTopBar === next.settings.permanentTopBar &&
+        prev.settings.buttonControlBar === next.settings.buttonControlBar &&
+        prev.settings.aspectRatio === next.settings.aspectRatio &&
+        prev.toolarea.toolAreaOpen === next.toolarea.toolAreaOpen
+      );
+    }
+  }
 )(withStyles(styles, { withTheme: true })(Democratic));
